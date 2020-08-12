16f157069ef05f235492b95700c74380a9c82466
Anastasia <anastasia.i2811@gmail.com>
2020-08-12 22:19:10 +0300

    Thymeleaf added

--- src/main/java/com/ana/coronavirustracker/controller/HomeController.java
+++ src/main/java/com/ana/coronavirustracker/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.ana.coronavirustracker.controller;
 
+import com.ana.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -10,8 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("stats", coronaVirusDataService.getAllStats());
         return "home";
     }
 }
--- src/main/java/com/ana/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/ana/coronavirustracker/services/CoronaVirusDataService.java
@@ -25,7 +25,11 @@ public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-    private List<LocationStats> stats = new ArrayList<>();
+    private List<LocationStats> allStats = new ArrayList<>();
+
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
 
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
@@ -48,6 +52,6 @@ public class CoronaVirusDataService {
             newStats.add(locationStat);
         }
 
-        this.stats = newStats;
+        this.allStats = newStats;
     }
 }
